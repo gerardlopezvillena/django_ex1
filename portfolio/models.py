364736df7c7e80c0ec822866c90d8a347a3d6894
Gerard López Villena <gerardlopezvillena@gmail.com>
2018-08-20 02:33:49 +0200

    Configurant Panell Administrador

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title=models.CharField(max_length=200)
-    description=models.TextField()
-    image=models.ImageField()
-    created=models.DateTimeField(auto_now_add=True) #al atribut es guarda la data de creacio del objecte de manera automatica
-    updated=models.DateTimeField(auto_now=True) #al atribut es guarda la data de la ultima modificacio del object
+    title=models.CharField(max_length=200,verbose_name="Títol")
+    description=models.TextField(verbose_name="Descripció")
+    image=models.ImageField(verbose_name="Imatge",upload_to="projects")
+    created=models.DateTimeField(auto_now_add=True,verbose_name="Data de Creació") #al atribut es guarda la data de creacio del objecte de manera automatica
+    updated=models.DateTimeField(auto_now=True,verbose_name="Data d'Edició") #al atribut es guarda la data de la ultima modificacio del object
 
+    class Meta:
+        verbose_name="projecte"
+        verbose_name_plural="projectes" #si no especifiquem aquesta variable, per defecte afegeix una S al nom definit (per defecte o al verbose_name)
+        ordering=["-created"] # posem un guio - per indicar que el ordre ha de ser a la inversa, en aquest cas no volem que sigui del registre mes antic al mes nou sino al contrari, del mes nou al mes antic
+
+    def __str__(self):
+        return(self.title)
--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'portfolio',
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media files
+MEDIA_URL='/media/' # indiquem la direccio publica
+MEDIA_ROOT=os.path.join(BASE_DIR,"media") # indiquem la ruta completa on django ha de trobar el fitxers
