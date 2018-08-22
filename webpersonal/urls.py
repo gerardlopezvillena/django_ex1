4e5f36af2633d8039f8c583b69a10b4ab7c97ab8
Gerard López Villena <gerardlopezvillena@gmail.com>
2018-08-22 02:22:49 +0200

    Fi Configuracio Panell Admin

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+from django.conf import settings
 
 urlpatterns = [
     path('',views.home,name='home'),
@@ -25,3 +26,7 @@ urlpatterns = [
     path('contact/',views.contact,name='contact'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static #aixo ens permetra servir fitxers estatics
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
