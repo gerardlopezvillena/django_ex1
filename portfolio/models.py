66c89f16efd3bec847dea34d7d6d6c31fc21f2ba
Gerard López Villena <gerardlopezvillena@gmail.com>
2018-08-24 00:08:29 +0200

    Final WEBPERSONAL

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title=models.CharField(max_length=200,verbose_name="Títol")
     description=models.TextField(verbose_name="Descripció")
     image=models.ImageField(verbose_name="Imatge",upload_to="projects")
+    link=models.URLField(verbose_name="Direcció Web",null=True,blank=True)
     created=models.DateTimeField(auto_now_add=True,verbose_name="Data de Creació") #al atribut es guarda la data de creacio del objecte de manera automatica
     updated=models.DateTimeField(auto_now=True,verbose_name="Data d'Edició") #al atribut es guarda la data de la ultima modificacio del object
 
